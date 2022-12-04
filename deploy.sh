22e4b7a7068f7639986276a88f273d4f1b8808b0
psiana011 <psiana011.endo@gmail.com>
2022-12-04 17:54:54 +0900

    設定ファイルを配置するスクリプトを追加

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+# starship
+cp -R starship/* $HOME/.config/starship
+
+# fish
+cp -R fish/* $HOME/.config/fish
+
+# helix
+cp -R helix/* $HOME/.config/helix
+
+# tmux
+cp -R tmux/* $HOME/.config/tmux
+
+#wezterm
+cp -R wezterm/* $HOME/.config/wezterm
