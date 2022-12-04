cae2a728aa91c9e25cbd0d56dce7dbee04f82243
psiana011 <psiana011.endo@gmail.com>
2022-12-05 04:03:33 +0900

    [fish] uutils-coreutils の PATH を追加

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,8 @@ case Darwin
     set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
     set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
     set -gx LIBCLANG_PATH "/opt/homebrew/opt/llvm/lib"
+    # uutils-coreutils
+    fish_add_path --path "/opt/homebrew/opt/uutils-coreutils/libexec/uubin"
 case Linux
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     set -gx LD_LIBRARY_PATH "/home/linuxbrew/.linuxbrew/lib" $LD_LIBRARY_PATH
@@ -20,6 +22,8 @@ case Linux
     set -gx LDFLAGS "-L/home/linuxbrew/.linuxbrew/opt/llvm/lib"
     set -gx CPPFLAGS "-I/home/linuxbrew/.linuxbrew/opt/llvm/include"
     set -gx LIBCLANG_PATH "/home/linuxbrew/.linuxbrew/opt/llvm/lib"
+    # uutils-coreutils
+    fish_add_path --path "/home/linuxbrew/.linuxbrew/opt/uutils-coreutils/libexec/uubin"
 end
 
 # cargo
