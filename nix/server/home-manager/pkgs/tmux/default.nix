b8ff94c4e90930d15013834df52e4bec54fdc431
psiana011 <psiana011.endo@gmail.com>
2025-01-14 03:15:07 +0900

    Upd: update tmux configuration
    Using catppuccin theme

--- nix/server/home-manager/pkgs/tmux/default.nix
+++ nix/server/home-manager/pkgs/tmux/default.nix
@@ -1,4 +1,16 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  tmux-catppuccin = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "tmux";
+    rev = "ba9bd88c98c81f25060f051ed983e40f82fdd3ba";
+    hash = "sha256-HegD89d0HUJ7dHKWPkiJCIApPY/yqgYusn7e1LDYS6c=";
+  };
+
+  pwd-command = "#{@catppuccin_directory_icon}#(sed -e 's|$HOME|~|' <(${pkgs.uutils-coreutils}/bin/uutils-echo #{pane_current_path}))";
+  git-branch-command = "#{?#(cd #{pane_current_path}; ${pkgs.git}/bin/git rev-parse --git-dir > /dev/null 2>&1; ${pkgs.uutils-coreutils}/bin/uutils-echo $?),, (#{@catppuccin_gitmux_icon}#(cd #{pane_current_path}; ${pkgs.git}/bin/git rev-parse --abbrev-ref HEAD))}";
+in
+{
   programs.tmux = {
     enable = true;
     aggressiveResize = true;
@@ -6,44 +18,52 @@
     clock24 = true;
     escapeTime = 1;
     keyMode = "vi";
+    customPaneNavigationAndResize = true;
     mouse = true;
     terminal = "tmux-256color";
+
     extraConfig = ''
-      set-option -g default-terminal "tmux-256color"
-      set-option -g terminal-overrides ",$TERM:Tc"
-      set-option -g renumber-windows on
+      set -g terminal-overrides ",$TERM:Tc"
+      set -g renumber-windows on
 
-      set-option -g status-interval 1
+      set -g status-interval 1
+      set -g status-position top
 
-      bind-key h select-pane -L
-      bind-key j select-pane -D
-      bind-key k select-pane -U
-      bind-key l select-pane -R
+      bind '"' split-window -c '#{pane_current_path}'
+      bind '%' split-window -h -c '#{pane_current_path}'
 
-      bind-key '"' split-window -c '#{pane_current_path}'
-      bind-key % split-window -h -c '#{pane_current_path}'
+      bind -T copy-mode-vi v   send-keys -X begin-selection
+      bind -T copy-mode-vi V   send-keys -X select-line
+      bind -T copy-mode-vi C-v send-keys -X rectangle-toggle \; send-keys -X begin-selection
+      bind -T copy-mode-vi y   send-keys -X copy-pipe-and-cancel "pbcopy"
 
-      bind-key -T copy-mode-vi v                send-keys -X begin-selection
-      bind-key -T copy-mode-vi V                send-keys -X select-line
-      bind-key -T copy-mode-vi C-v      send-keys -X rectangle-toggle \; send-keys -X begin-selection
-      bind-key -T copy-mode-vi y                send-keys -X copy-pipe-and-cancel "pbcopy"
+      set -g @catppuccin_flavor "frappe" # latte, frappe, macchiato or mocha
+      set -g @catppuccin_window_status_style "basic"
+      set -g @catppuccin_status_left_separator "█"
+      set -g @catppuccin_status_right_separator "█ "
+      set -g @catppuccin_status_connect_separator "no"
 
-      set-option -g status-bg "colour238"
-      set-option -g status-fg "colour255"
-      set-option -g status-position top
-      set-option -g status-justify left
-      set-option -g window-status-current-style underscore
+      set -g @catppuccin_window_current_text " #{pane_current_command}"
+      set -g @catppuccin_window_text " #{pane_current_command}"
 
-      set-option -g status-left-length 50
-      set-option -g status-left "#[fg=colour255,bg=colour241] Session:#S #[default] #[fg=colour255,bg=colour238]"
+      set -g @catppuccin_session_icon " tmux "
+      set -g @catppuccin_session_text " #S"
 
-      set-option -g status-right-length 100
-      set-option -g status-right "#[fg=colour255,bg=colour241] #(whoami) | #h | %Y-%m-%d(%a) %H:%M "
+      set -g status-left-length 100
+      set -g status-left "#{E:@catppuccin_status_session}"
 
-      set-option -g pane-border-status bottom
-      set-option -g pane-border-style fg=colour238
-      set-option -g pane-active-border-style fg=colour45
-      set-option -g pane-border-format " Pane:#{pane_index} [#{pane_current_path}] #{?#(cd #{pane_current_path}; git rev-parse --git-dir > /dev/null 2>&1; echo $?),,(#(cd #{pane_current_path}; git rev-parse --abbrev-ref HEAD)) }"
+      set -g status-right-length 100
+      set -g status-right ""
+      set -ga status-right "#{E:@catppuccin_status_user}"
+      set -ga status-right "#{E:@catppuccin_status_host}"
+
+      set -g pane-border-status bottom
+      # set -g @catppuccin_pane_status_enabled "yes"
+      set -g @catppuccin_pane_border_status "yes"
+      set -g @catppuccin_pane_border_style "fg=colour238" # デフォルトだと明るすぎる
+      set -g pane-border-format " #{pane_index}: ${pwd-command}${git-branch-command} "
+
+      run ${tmux-catppuccin}/catppuccin.tmux
     '';
   };
 }
