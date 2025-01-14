0c934c4a159095496bb3d4f0cdc663b134095de4
psiana011 <psiana011.endo@gmail.com>
2025-01-14 12:37:34 +0900

    Upd(starship, tmux): Set catppuccin_mocha as theme

--- nix/server/home-manager/pkgs/starship/default.nix
+++ nix/server/home-manager/pkgs/starship/default.nix
@@ -1,25 +1,46 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  catppuccin-starship = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "starship";
+    rev = "e99ba6b210c0739af2a18094024ca0bdf4bb3225";
+    hash = "sha256-1w0TJdQP5lb9jCrCmhPlSexf0PkAlcz8GBDEsRjPRns=";
+  };
+in
+{
   programs.starship = {
     enable = true;
     enableBashIntegration = false; # bash 起動時は starship を off にする
 
     settings = {
+      palette = "catppuccin_mocha";
+
+      palettes = {
+        catppuccin_frappe = (builtins.fromTOML (builtins.readFile "${catppuccin-starship}/themes/frappe.toml")).palettes.catppuccin_frappe;
+        catppuccin_latte = (builtins.fromTOML (builtins.readFile "${catppuccin-starship}/themes/latte.toml")).palettes.catppuccin_latte;
+        catppuccin_macchiato = (builtins.fromTOML (builtins.readFile "${catppuccin-starship}/themes/macchiato.toml")).palettes.catppuccin_macchiato;
+        catppuccin_mocha = (builtins.fromTOML (builtins.readFile "${catppuccin-starship}/themes/mocha.toml")).palettes.catppuccin_mocha;
+      };
+
       status = {
         disabled = false;
         pipestatus = true;
-        pipestatus_format = "\[$pipestatus\] => [$symbol$common_meaning$signal_name$maybe_int]($style) "; # デフォルトの最後に空白を入れただけ
+        pipestatus_format = "\\[$pipestatus\\] => [$symbol$common_meaning$signal_name$maybe_int]($style) "; # デフォルトの最後に空白を入れただけ
+        pipestatus_segment_format = "[$symbol$status]($style)";
         symbol = "×";
       };
 
+      # 色合いは極力 https://github.com/helix-editor/helix/blob/master/runtime/themes/catppuccin_mocha.toml に合わせる
       character = {
-        format = "$symbol \\$ ";
-        success_symbol = "[ INS ](fg:black bg:green)";
-        error_symbol = "[ INS ](fg:black bg:green)";
-        vimcmd_symbol = "[ NOR ](fg:black bg:blue)";
-        vimcmd_replace_one_symbol = "[ REP ](fg:black bg:red)";
-        vimcmd_replace_symbol = "[ REP ](fg:black bg:red)";
-        vimcmd_visual_symbol = "[ SEL ](fg:black bg:purple)";
+        success_symbol = "[ INS ](fg:base bg:green)";
+        error_symbol = "[ INS ](fg:base bg:green)";
+        vimcmd_symbol = "[ NOR ](fg:base bg:lavender)";
+        vimcmd_replace_one_symbol = "[ REP ](fg:base bg:red)";
+        vimcmd_replace_symbol = "[ REP ](fg:base bg:red)";
+        vimcmd_visual_symbol = "[ SEL ](fg:base bg:flamingo)";
       };
+
+      format = "$all$character$status\\$ ";
     };
   };
 }
