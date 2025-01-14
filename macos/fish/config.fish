e165af83dd041399d9f6e458c5107dee696b3a7f
psiana011 <psiana011.endo@gmail.com>
2025-01-14 12:57:40 +0900

    Upd(macos): Update fish configuration

--- macos/fish/config.fish
+++ macos/fish/config.fish
@@ -1,24 +1,21 @@
 set -gx HOME (realpath $HOME)
 set -gx LANG "ja_JP.UTF-8"
-set -gx COLORTERM "truecolor"
+set -gx COLORTERM truecolor
 
 # homebrew
 eval (/opt/homebrew/bin/brew shellenv)
-set -gx LD_LIBRARY_PATH "/opt/homebrew/lib" $LD_LIBRARY_PATH
+set -gx LD_LIBRARY_PATH /opt/homebrew/lib $LD_LIBRARY_PATH
+
 ## llvm
-fish_add_path --path "/opt/homebrew/opt/llvm/bin"
-set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
-set -gx LIBCLANG_PATH "/opt/homebrew/opt/llvm/lib"
-
-# cargo
-fish_add_path --path "$HOME/.cargo/bin"
-
-# gpg
-set -gx GPG_TTY (tty)
+fish_add_path --path /opt/homebrew/opt/llvm/bin
+set -gx LDFLAGS -L/opt/homebrew/opt/llvm/lib
+set -gx CPPFLAGS -I/opt/homebrew/opt/llvm/include
+set -gx LIBCLANG_PATH /opt/homebrew/opt/llvm/lib
 
 # default editor
-set -gx EDITOR "hx"
+set -gx EDITOR hx
+set -gx VISUAL hx
+set -gx GIT_EDITOR hx
 
 # man on bat
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
@@ -28,6 +25,9 @@ if status is-interactive
 
     # vi keybind
     fish_vi_key_bindings
+
+    # theme
+    yes | fish_config theme save "Catppuccin Mocha"
 
     # starship
     set -gx STARSHIP_CONFIG "$HOME/.config/starship/config.toml"
