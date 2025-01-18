46110cf17559098f7910c769a3c583ceecedc5d5
psiana011 <psiana011.endo@gmail.com>
2025-01-18 18:25:52 +0900

    Add(macos): Add Brewfile for homebrew bundle

--- macos/fish/conf.d/homebrew.fish
+++ macos/fish/conf.d/homebrew.fish
@@ -1,2 +1,4 @@
 eval (/opt/homebrew/bin/brew shellenv)
 set -gx LD_LIBRARY_PATH /opt/homebrew/lib $LD_LIBRARY_PATH
+set -gx HOMEBREW_BUNDLE_FILE "$HOME/.config/homebrew/Brewfile"
+set -gx HOMEBREW_BUNDLE_DUMP_NO_VSCODE 1
