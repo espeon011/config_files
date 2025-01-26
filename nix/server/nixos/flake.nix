7f254302319b804a12d224578dd5cea76d6c5d58
psiana011 <psiana011.endo@gmail.com>
2025-01-26 23:25:59 +0900

    Add(nixos): Add flake-programs-sqlite for NixOS configuration

--- nix/server/nixos/flake.nix
+++ nix/server/nixos/flake.nix
@@ -1,15 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-programs-sqlite = {
+      url = "github:wamserma/flake-programs-sqlite";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, flake-programs-sqlite, ... }:
     let
       hostname = (import ./host.nix).networking.hostName;
     in
     {
       nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules = [
+          flake-programs-sqlite.nixosModules.programs-sqlite
+          ./configuration.nix
+        ];
       };
     };
 }
