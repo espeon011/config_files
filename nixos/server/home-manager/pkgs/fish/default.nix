b2284db18c76f71ab98f16dfa96b8bcd7492c303
psiana011 <psiana011.endo@gmail.com>
2026-02-04 23:07:17 +0900

    nixos server: update fish theme configuration

--- nixos/server/home-manager/pkgs/fish/default.nix
+++ nixos/server/home-manager/pkgs/fish/default.nix
@@ -1,16 +1,9 @@
-{pkgs, ...}: let
-  theme =
-    if builtins.pathExists ./catppuccin.nix
-    then "Catppuccin Mocha"
-    else "fish default";
-in {
-  imports = [./catppuccin.nix];
-
+{pkgs, ...}: {
   programs.fish = {
     enable = true;
     shellInit = "set -x SHELL $(which fish)";
     interactiveShellInit = "
-      yes | fish_config theme save '${theme}'
+      yes | fish_config theme choose catppuccin-mocha
       fish_vi_key_bindings
     ";
 
@@ -19,15 +12,10 @@ in {
         name = "plugin-git";
         src = pkgs.fishPlugins.plugin-git.src;
       }
-
-      ## 最終的には starship をやめて hydro に置き換えたい
-      ## そのために starship でどのような機能があったかを確認しておこう
-      ## - 中断されたタスクがある場合に出るマーク: ✦
-      ## - プログラミング言語のバージョン(これは別にいらないかな...)
-      ## - nix env ($IN_NIX_SHELL で判定できる)
-      # { name = "hydro"; src = pkgs.fishPlugins.hydro.src; }
-
-      # { name = "tide"; src = pkgs.fishPlugins.tide.src; }
+      {
+        name = "sponge";
+        src = pkgs.fishPlugins.sponge.src;
+      }
     ];
   };
 
